13f8ca5f7dd2654f2e54312266dccc66accec0c8
SiuKeung <siu_keung@outlook.com>
2018-07-28 15:48:39 +0800

    增加了exception包，存放用于处理异常的类； 增加了ExceptionHandler类，所有Controller方法中抛出的异常都会被该类捕获进行处理。

---
+++ src/main/java/com/oocl/springBootApiPractice2/exception/handler/ExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.oocl.springBootApiPractice2.exception.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Dylan Wei
+ * @date 2018-07-28 15:46
+ */
+@ControllerAdvice
+@ResponseBody
+public class ExceptionHandler {
+
+}
